a3ba36bd8a990b3576d3f6a3f763c53ceb3f2340
Alex <alej.alvarado94@gmail.com>
2019-09-05 17:04:41 -0500

    why

---
+++ precache-manifest.435148289cd0aa98b76e30ca1e65dc5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "422738a040da17339e252743ecebde51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c4807eccab1f38a8a0b",
+    "url": "/static/css/main.3b47120a.chunk.css"
+  },
+  {
+    "revision": "3c2571aa12fcd14c2842",
+    "url": "/static/js/2.369e1fb1.chunk.js"
+  },
+  {
+    "revision": "2c4807eccab1f38a8a0b",
+    "url": "/static/js/main.f50f3d90.chunk.js"
+  },
+  {
+    "revision": "ee3fd842ae1f9d5fbc27",
+    "url": "/static/js/runtime~main.c236d50e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
